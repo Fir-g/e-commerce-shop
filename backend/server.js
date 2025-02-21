7c7d7ff81f8791a6702f3460259e9bf804f535c0
Firoj Ahamad <firojahamad5072002@gmail.com>
2025-02-21 16:16:44 +0530

    cart and order

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // allow you to parse the body of the request
+app.use(express.json({limit:"10mb"})); // allow you to parse the body of the request
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
 // import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Navigate,Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import Navbar from './components/Navbar';
 import { Toaster } from 'react-hot-toast';
+import { useUserStore } from './stores/useUserStore';
+import { useEffect } from 'react';
+import LoadingSpinner from './components/LoadingSpinner';
+import AdminPage from './pages/AdminPage';
+import CategoryPage from './pages/CategoryPage';
+import CartPage from './pages/CartPage';
 
 function App() {
+  const { user, checkAuth,checkingAuth }=useUserStore();
+
+  useEffect(()=>{
+    checkAuth();
+  }, [checkAuth]);
+
+  if (checkingAuth) return <LoadingSpinner />;
+
   return (
     <div  className='min-h-screen bg-gray-900 text-white overflow-hidden '   >
       {/* Background gradient */}
@@ -23,8 +37,12 @@ function App() {
         
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='/signup' element={<SignUpPage />} />
-          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to='/' />} />
+
+          <Route path='/login' element={!user ? <LoginPage /> : <Navigate to='/' />}  />
+          <Route path='/secret-dashboard' element={user?.role=== "admin"? <AdminPage/>:  <Navigate to='/login' />} />
+          <Route path='/category/:category' element={ <CategoryPage />} />
+          <Route path='/cart' element={user ? <CartPage /> :<Navigate to='/login' />} />
         </Routes>
       </div>
       <Toaster />
